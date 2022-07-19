b5f03ce837da7bf1303ae0efb1306349d70550bb
KateRemp <rempelkate@gmail.com>
2022-07-19 21:48:14 +0200

    add .eslintrc.js prettier

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,7 @@
-import * as users from "./fake.api/user.api"
-
-//объект API у которого есть объект users у которого есть метод fetchAll который возвращает массив объектов
-//console.log(api)// {users:Module}
+import * as users from "./fake.api/user.api";
+// объект API у которого есть объект users у которого есть метод fetchAll который возвращает массив объектов
+// console.log(api)//{users:Module}
 const API = {
-	users
-}
-export default API
+    users
+};
+export default API;
